8b14833e42d7fd2cef54306cc700b71b5818114d
hellozjf <908686171@qq.com>
2025-06-13 21:33:01 +0800

    第三章——Column

--- 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ColumnScreen.kt
+++ 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ColumnScreen.kt
@@ -34,21 +34,47 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
 
 @Composable
 fun ColumnScreen() {
-  MyColumn()
+    MyColumn()
 
-  BackButtonHandler {
-    JetFundamentalsRouter.navigateTo(Screen.Navigation)
-  }
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Preview
+@Composable
+fun MyColumnPreview() {
+    MyColumn()
 }
 
 @Composable
 fun MyColumn() {
-  //TODO write your code here
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        THREE_ELEMENT_LIST.forEach { textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 22.sp
+            )
+        }
+    }
 }
