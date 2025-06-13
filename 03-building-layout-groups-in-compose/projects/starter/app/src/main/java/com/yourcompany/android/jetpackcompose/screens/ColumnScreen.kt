e8944d773d44db2b4dc601f8f88c1e78723ad57d
hellozjf <908686171@qq.com>
2025-06-13 22:50:28 +0800

    第三章——Surface

--- 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ColumnScreen.kt
+++ 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ColumnScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -73,7 +74,8 @@ fun MyColumn() {
         THREE_ELEMENT_LIST.forEach { textResId ->
             Text(
                 text = stringResource(id = textResId),
-                fontSize = 22.sp
+                fontSize = 22.sp,
+                // color = Color.Black
             )
         }
     }
