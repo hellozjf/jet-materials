b7c0a04e9d87b59ce6d171fea5a7937aec37fa77
hellozjf <908686171@qq.com>
2025-06-14 20:26:15 +0800

    第四章——horizontalScroll

--- 04-building-lists-with-jetpack-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ScrollingScreen.kt
+++ 04-building-lists-with-jetpack-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ScrollingScreen.kt
@@ -37,7 +37,9 @@ package com.yourcompany.android.jetpackcompose.screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -71,7 +73,7 @@ fun MyScrollingScreenPreview() {
 
 @Composable
 fun MyScrollingScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
         BookImage(R.drawable.advanced_architecture_android, R.string.advanced_architecture_android)
         BookImage(R.drawable.kotlin_aprentice, R.string.kotlin_apprentice)
         BookImage(R.drawable.kotlin_coroutines, R.string.kotlin_coroutines)
