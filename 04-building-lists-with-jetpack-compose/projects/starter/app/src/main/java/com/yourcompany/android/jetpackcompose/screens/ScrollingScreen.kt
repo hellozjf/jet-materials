f12e19164bf1cea0ccb499290751c3994042170d
hellozjf <908686171@qq.com>
2025-06-15 19:21:05 +0800

    第四章——LazyColumn和LazyRow

--- 04-building-lists-with-jetpack-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ScrollingScreen.kt
+++ 04-building-lists-with-jetpack-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ScrollingScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
