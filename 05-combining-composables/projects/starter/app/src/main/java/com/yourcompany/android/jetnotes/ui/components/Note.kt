f5473b52b2075a29f3543dcff3799fac97149026
hellozjf <908686171@qq.com>
2025-06-16 13:49:54 +0800

    第五章——添加Note

---
+++ 05-combining-composables/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/components/Note.kt
@@ -0,0 +1,15 @@
+package com.yourcompany.android.jetnotes.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Note() {
+
+}
+
+@Preview
+@Composable
+private fun NotePreview() {
+    Note()
+}
