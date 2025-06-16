be7bef40fb2de658459f986f6618b49db9d56b03
hellozjf <908686171@qq.com>
2025-06-16 21:01:40 +0800

    第六章——添加 NoteColor

---
+++ 06-using-compose-modifiers/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/NoteColor.kt
@@ -0,0 +1,59 @@
+package com.raywenderlich.android.jetnotes.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteColor(
+    color: Color,
+    size: Dp,
+    padding: Dp = 0.dp,
+    border: Dp
+) {
+    Box(
+        modifier = Modifier
+            // 先设置外边距
+            .padding(padding)
+            // 然后设置内部大小
+            .size(size)
+            // 这里设置 padding 的话，就是设置内边距了
+            // 把画笔改成圆形画笔
+            .clip(CircleShape)
+            // 画一个红色圈上去
+            .background(color)
+            // 画边框
+            .border(
+                // 边框线宽 2dp，黑色
+                BorderStroke(
+                    border,
+                    SolidColor(Color.Black)
+                ),
+                // 圆形
+                CircleShape
+            )
+    )
+}
+
+@Preview
+@Composable
+fun NoteColorPreview() {
+    NoteColor(
+        color = Color.Red,
+        size = 40.dp,
+        padding = 4.dp,
+        border = 2.dp
+    )
+}
