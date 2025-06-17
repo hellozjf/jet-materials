ac62ae0c2f08210d08f569bb963fe53eccffbc76
hellozjf <908686171@qq.com>
2025-06-17 20:03:54 +0800

    第7章，创建 NotesScreen

--- 07-managing-state-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
+++ 07-managing-state-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
@@ -42,10 +42,13 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.yourcompany.android.jetnotes.routing.Screen
 import com.yourcompany.android.jetnotes.theme.JetNotesTheme
 import com.yourcompany.android.jetnotes.ui.components.AppDrawer
-import com.yourcompany.android.jetnotes.ui.components.Note
+import com.yourcompany.android.jetnotes.ui.screens.NotesScreen
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModel
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModelFactory
 import kotlinx.coroutines.launch
@@ -73,6 +76,7 @@ class MainActivity : AppCompatActivity() {
       JetNotesTheme {
         val coroutineScope = rememberCoroutineScope()
         val scaffoldState: ScaffoldState = rememberScaffoldState()
+        val navController = rememberNavController()
 
         Scaffold(
           scaffoldState = scaffoldState,
@@ -88,7 +92,15 @@ class MainActivity : AppCompatActivity() {
             )
           },
           content = {
-            Note()
+            // Note()
+            NavHost(
+              navController = navController,
+              startDestination = Screen.Notes.route
+            ) {
+              composable(Screen.Notes.route) {
+                NotesScreen(viewModel)
+              }
+            }
           }
         )
       }
