1a7399e2923be7261eb306ffff5495c94feecfaa
hellozjf <908686171@qq.com>
2025-06-18 21:43:02 +0800

    第8章，添加 NoteCheckOption

--- 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
+++ 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
@@ -43,6 +43,7 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -191,6 +192,41 @@ private fun PickedColor(color: ColorModel) {
 @Composable
 fun PickedColorPreview() {
   PickedColor(ColorModel.DEFAULT)
+}
+
+@Composable
+private fun NoteCheckOption(
+  isChecked: Boolean,
+  onCheckedChange: (Boolean) -> Unit
+) {
+  Row(
+    Modifier
+      .padding(8.dp)
+      .padding(top = 16.dp)
+  ) {
+    Text(
+      text = "Can note be checked off?",
+      modifier = Modifier
+        .weight(1f)
+        .align(Alignment.CenterVertically)
+    )
+    Switch(
+      checked = isChecked,
+      onCheckedChange = onCheckedChange,
+      modifier = Modifier.padding(start = 8.dp)
+    )
+  }
+}
+
+@Preview(
+  showBackground = true
+)
+@Composable
+fun NoteCheckOptionPreview() {
+  NoteCheckOption(
+    isChecked = true,
+    onCheckedChange = {}
+  )
 }
 
 @Composable
