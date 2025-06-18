3b747c75191bee372496bf089f7dccc984519b93
hellozjf <908686171@qq.com>
2025-06-18 23:26:51 +0800

    第8章，增加深色主题颜色

--- 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/theme/Theme.kt
+++ 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/theme/Theme.kt
@@ -36,22 +36,25 @@ package com.yourcompany.android.jetnotes.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 private val LightThemeColors = lightColors(
   primary = green,
   primaryVariant = greenDark,
-  secondary = green
+  secondary = red
 )
 
-private val DarkThemeColors = lightColors(
+private val DarkThemeColors = darkColors(
   primary = green,
   primaryVariant = greenDark,
-  secondary = green
+  secondary = red,
+  onPrimary = Color.White
 )
 
 /**
