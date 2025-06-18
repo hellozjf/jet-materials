762724acd82ebe131038dc901da8b63752eb99e3
hellozjf <908686171@qq.com>
2025-06-18 22:37:50 +0800

    第8章，使用 BottomDrawer 打开 ColorPicker

--- 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
+++ 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
@@ -41,6 +41,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.BottomDrawer
+import androidx.compose.material.BottomDrawerState
+import androidx.compose.material.BottomDrawerValue
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -54,9 +58,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberBottomDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -72,13 +78,19 @@ import com.yourcompany.android.jetnotes.domain.model.NoteModel
 import com.yourcompany.android.jetnotes.ui.components.NoteColor
 import com.yourcompany.android.jetnotes.util.fromHex
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SaveNoteScreen(
   viewModel: MainViewModel,
   onNavigateBack: () -> Unit = {}
 ) {
   val noteEntry: NoteModel by viewModel.noteEntry.observeAsState(NoteModel())
+
+  val colors: List<ColorModel> by viewModel.colors.observeAsState(listOf())
+  val bottomDrawerState: BottomDrawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
+  val coroutineScope = rememberCoroutineScope()
 
   Scaffold(
     topBar = {
@@ -90,7 +102,11 @@ fun SaveNoteScreen(
           viewModel.saveNote(noteEntry)
           onNavigateBack()
         },
-        onOpenColorPickerClick = {},
+        onOpenColorPickerClick = {
+          coroutineScope.launch {
+            bottomDrawerState.open()
+          }
+        },
         onDeleteNoteClick = {
           viewModel.moveNoteToTrash(noteEntry)
           onNavigateBack()
@@ -98,13 +114,26 @@ fun SaveNoteScreen(
       )
     }
   ) { paddingValues ->
-    SaveNoteContent(
+    BottomDrawer(
       modifier = Modifier.padding(paddingValues),
-      note = noteEntry,
-      onNoteChange = { updateNoteEntry ->
-        viewModel.onNoteEntryChange(updateNoteEntry)
+      drawerState = bottomDrawerState,
+      drawerContent = {
+        ColorPicker(
+          colors = colors,
+          onColorSelect = { color ->
+            val newNoteEntry = noteEntry.copy(color = color)
+            viewModel.onNoteEntryChange(newNoteEntry)
+          }
+        )
       }
-    )
+    ) {
+      SaveNoteContent(
+        note = noteEntry,
+        onNoteChange = { updateNoteEntry ->
+          viewModel.onNoteEntryChange(updateNoteEntry)
+        }
+      )
+    }
   }
 }
 
