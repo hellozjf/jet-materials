838c7540cc8cee572a275aca66b64d3dfc360375
hellozjf <908686171@qq.com>
2025-06-19 18:47:24 +0800

    第9章，AppDrawer添加图片文字分隔线

--- 09-using-constraintsets-in-composables/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/appdrawer/AppDrawer.kt
+++ 09-using-constraintsets-in-composables/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/appdrawer/AppDrawer.kt
@@ -36,11 +36,13 @@ package com.yourcompany.android.jetreddit.appdrawer
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -48,6 +50,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -76,12 +80,44 @@ fun AppDrawer(
   }
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun AppDrawerHeaderPreview() {
+  AppDrawerHeader()
+}
+
 /**
  * Represents app drawer header with the icon and the app name
  */
 @Composable
 private fun AppDrawerHeader() {
-  //TODO add your code here
+  Column(
+    modifier = Modifier.fillMaxWidth(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+    Image(
+      modifier = Modifier
+        .padding(16.dp)
+        .size(50.dp),
+      imageVector = Icons.Filled.AccountCircle,
+      colorFilter = ColorFilter.tint(Color.LightGray),
+      contentScale = ContentScale.Fit,
+      alignment = Alignment.Center,
+      contentDescription = stringResource(id = R.string.android)
+    )
+    Text(
+      text = stringResource(R.string.default_username),
+      color = MaterialTheme.colors.primaryVariant
+    )
+    Divider(
+      modifier = Modifier.padding(
+        start = 16.dp,
+        end = 16.dp,
+        top = 16.dp
+      ),
+      color = MaterialTheme.colors.onSurface.copy(alpha = .2f)
+    )
+  }
 }
 
 @Composable
