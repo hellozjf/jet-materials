ef735be45cc7595def390692aaa12b8f49ae94c1
hellozjf <908686171@qq.com>
2025-06-20 18:55:20 +0800

    第10章——增加 Community

--- 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/SubredditsScreen.kt
+++ 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/SubredditsScreen.kt
@@ -36,16 +36,21 @@ package com.yourcompany.android.jetreddit.screens
 import androidx.annotation.StringRes
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -220,8 +225,36 @@ fun SubredditDescription(modifier: Modifier, @StringRes descriptionStringRes: In
 }
 
 @Composable
-fun Community(text: String, modifier: Modifier = Modifier) {
-  //TODO add your code here
+fun Community(
+  modifier: Modifier = Modifier,
+  text: String,
+  onCommunityClicked: () -> Unit = {}
+) {
+  Row(
+    modifier = modifier
+      .padding(start = 16.dp, top = 16.dp)
+      .fillMaxWidth()
+      .clickable(
+        onClick = { onCommunityClicked() }
+      )
+  ) {
+    Image(
+      modifier = modifier
+        .size(24.dp)
+        .clip(CircleShape),
+      bitmap = ImageBitmap.imageResource(id = R.drawable.subreddit_placeholder),
+      contentDescription = stringResource(id = R.string.community_icon)
+    )
+    Text(
+      modifier = modifier
+        .padding(start = 16.dp)
+        .align(alignment = Alignment.CenterVertically),
+      fontSize = 10.sp,
+      color = MaterialTheme.colors.primaryVariant,
+      text = text,
+      fontWeight = FontWeight.Bold
+    )
+  }
 }
 
 @Composable
@@ -241,10 +274,14 @@ fun SubredditPreview() {
   Subreddit(SubredditModel.DEFAULT_SUBREDDIT)
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CommunityPreview() {
-  Community(stringResource(id = R.string.kodeco_com))
+  Community(
+    modifier = Modifier,
+    stringResource(id = R.string.kodeco_com),
+    onCommunityClicked = {}
+  )
 }
 
 @Preview
